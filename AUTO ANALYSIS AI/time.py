00a7e1dc0c95630e0eaaf447e8858f158a7ef1bd
Nitesh-Jakhar <niteshjakhar13@gmail.com>
2024-02-20 10:20:37 +0530

    I am adding this

---
+++ AUTO ANALYSIS AI/time.py
@@ -0,0 +1,14 @@
+import datetime
+
+# Get current date and time
+current_datetime = datetime.datetime.now()
+
+# Print current date and time
+print("Current Date and Time:", current_datetime)
+
+# Format current date and time as a string
+formatted_datetime = current_datetime.strftime("%m-%d %H:%M:%S IST %Y")
+
+# Print formatted date and time
+print("Formatted Date and Time:", formatted_datetime)
+
